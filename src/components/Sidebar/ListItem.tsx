868d1b6e68cad61b2a63ddbb859541268ca41e10
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-09 20:36:28 -0300

    Makes ListItem hover effect make the text brighter

--- src/components/Sidebar/ListItem.tsx
+++ src/components/Sidebar/ListItem.tsx
@@ -8,7 +8,7 @@ function ListItem({ fileName, selected }: SidebarFileListItemProps) {
     <div
       className={`${
         selected ? "bg-neutral-700/80 text-neutral-200" : "text-neutral-400"
-      } select-none font-medium overflow-hidden w-full flex items-center px-6 py-0.5 rounded-xs hover:bg-neutral-700/80 transition-colors linear`}
+      } select-none font-medium overflow-hidden w-full flex items-center px-6 py-0.5 rounded-xs hover:bg-neutral-700/80 hover:text-neutral-200 transition-colors linear`}
     >
       <span className="truncate">{fileName}</span>
     </div>
