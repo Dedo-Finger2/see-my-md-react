83bc9eeb37db2001803255a137b3fbe1d451b769
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-09 19:12:19 -0300

    Uses Sidebar component in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     // Wrapper
     <div className="grid grid-cols-[300px_1fr]">
       {/* Sidebar */}
-      <div className="bg-neutral-800 h-screen"></div>
+      <div className="bg-neutral-800 h-screen">
+        <Sidebar />
+      </div>
       {/* Content */}
       <div className="h-screen"></div>
     </div>
