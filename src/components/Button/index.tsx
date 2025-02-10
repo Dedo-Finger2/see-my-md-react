ea40bfb0534cae7b219d4e92da874d199bf26492
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-10 15:50:34 -0300

    Adds onClick prop on Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,13 @@
 type ButtonProps = {
   content: string;
   type: "button" | "submit";
+  onClick?: () => void;
 };
 
-function Button({ content, type }: ButtonProps) {
+function Button({ content, type, onClick }: ButtonProps) {
   return (
     <button
+      onClick={onClick}
       type={type}
       className="bg-violet-500 text-violet-50 font-medium text-sm rounded-sm py-1 hover:bg-violet-400"
     >
