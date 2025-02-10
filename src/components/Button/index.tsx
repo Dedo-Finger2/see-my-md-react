c5a61f0b82cd4672040376e74ba41d35e643b251
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-10 15:16:20 -0300

    Adds new prop to Button where you can specify the type of it

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,14 @@
 type ButtonProps = {
   content: string;
+  type: "button" | "submit";
 };
 
-function Button({ content }: ButtonProps) {
+function Button({ content, type }: ButtonProps) {
   return (
-    <button className="bg-violet-500 text-violet-50 font-medium text-sm rounded-sm py-1 hover:bg-violet-400">
+    <button
+      type={type}
+      className="bg-violet-500 text-violet-50 font-medium text-sm rounded-sm py-1 hover:bg-violet-400"
+    >
       {content}
     </button>
   );
