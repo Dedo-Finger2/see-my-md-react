2f9f2d9e31bb7841bb581168603dd68a37621547
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-10 09:27:06 -0300

    Adds PlatformForm component

---
+++ src/components/Config/PlatformForm.tsx
@@ -0,0 +1,21 @@
+import Button from "../Button";
+import Input from "../Input";
+
+function PlatformForm() {
+  return (
+    <div className="flex flex-col px-14 w-full">
+      <form className="flex flex-col gap-4">
+        <Input
+          type="text"
+          placeholder="Teste..."
+          labelContent="Teste"
+          descriptionContent="opa..."
+        />
+
+        <Button content="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default PlatformForm;
