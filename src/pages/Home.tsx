83947112a6236ad9ce35372fc7300a3abb32962f
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-10 16:15:38 -0300

    Ensures users cannot access HomePage without a platform data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,23 @@ import FileContent from "./../components/FileContent";
 import Sidebar from "./../components/Sidebar";
 import Header from "./../components/FileContent/Header";
 
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+import checkConfigData from "../api/middlewares/check-config-data";
+import checkApiKey from "../api/middlewares/check-api-key";
+
 function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let err;
+    err = checkApiKey();
+    if (err) navigate("/config");
+    err = checkConfigData();
+    if (err) navigate("/config");
+  }, []);
+
   return (
     // Wrapper
     <div className="grid grid-cols-[300px_1fr]">
