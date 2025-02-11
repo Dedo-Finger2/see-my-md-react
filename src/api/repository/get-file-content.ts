e6429661fb4a5bbe7f48dc8209ad06f736e6dc28
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-11 12:03:35 -0300

    Handles temp. getting files from folders. recommended to implement a file tree soon

--- src/api/repository/get-file-content.ts
+++ src/api/repository/get-file-content.ts
@@ -3,46 +3,47 @@ import { marked } from "marked";
 import applyStylesToMarkdownTurnedHtml from "../utils/apply-styles-to-markdown-turned-html";
 
 type Data = {
-  apiKey: string;
-  username: string;
-  repoName: string;
-  branchName: string;
-  filePath: string;
+	apiKey: string;
+	username: string;
+	repoName: string;
+	branchName: string;
+	filePath: string;
 };
 
 export type RepoFileContent = {
-  blobId: string;
-  commitId: string;
-  content: string;
-  size: number;
+	blobId: string;
+	commitId: string;
+	content: string;
+	size: number;
 };
 
 async function getFileContent({
-  apiKey,
-  branchName,
-  repoName,
-  username,
-  filePath,
+	apiKey,
+	branchName,
+	repoName,
+	username,
+	filePath,
 }: Data) {
-  const projectPath = encodeURIComponent(`${username}/${repoName}`);
-  const url = `https://gitlab.com/api/v4/projects/${projectPath}/repository/files/${filePath}?ref=${branchName}`;
-  const response = await fetch(url, {
-    headers: { "PRIVATE-TOKEN": apiKey },
-  });
-  if (!response.ok) {
-    console.error(await response.json());
-    throw new Error("Falha na requisição");
-  }
-  const body = await response.json();
-  const fileContent: RepoFileContent = {
-    blobId: body.blob_id,
-    commitId: body.commit_id,
-    content: applyStylesToMarkdownTurnedHtml(
-      await marked.parse(Base64.decode(body.content))
-    ),
-    size: body.size,
-  };
-  return fileContent;
+	const projectPath = encodeURIComponent(`${username}/${repoName}`);
+	const encodedFilePath = encodeURIComponent(filePath);
+	const url = `https://gitlab.com/api/v4/projects/${projectPath}/repository/files/${encodedFilePath}?ref=${branchName}`;
+	const response = await fetch(url, {
+		headers: { "PRIVATE-TOKEN": apiKey },
+	});
+	if (!response.ok) {
+		console.error(await response.json());
+		throw new Error("Falha na requisição");
+	}
+	const body = await response.json();
+	const fileContent: RepoFileContent = {
+		blobId: body.blob_id,
+		commitId: body.commit_id,
+		content: applyStylesToMarkdownTurnedHtml(
+			await marked.parse(Base64.decode(body.content))
+		),
+		size: body.size,
+	};
+	return fileContent;
 }
 
 export default getFileContent;
