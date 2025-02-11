c66565c14c20e77835b748dda6fc7da137fdc2ac
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-11 10:31:08 -0300

    Makes SupportedPlatforms grid responsive

--- src/components/Config/SuportedPlatforms.tsx
+++ src/components/Config/SuportedPlatforms.tsx
@@ -2,18 +2,25 @@ import { FaGitlab } from "react-icons/fa6";
 import PlatformCard from "./PlatformCard";
 
 function SuportedPlatforms() {
-  return (
-    <div className="grid grid-cols-3 gap-4">
-      {/* Single Platform */}
-      <PlatformCard
-        icon={<FaGitlab />}
-        title="Gitlab"
-        isDisabled={false}
-        description="Lorem ipsum dolor sit amet."
-        isSelected={false}
-      />
-    </div>
-  );
+	return (
+		<div className="grid grid-cols-1 place-items-center lg:grid-cols-2 xl:grid-cols-3 gap-4">
+			{/* Single Platform */}
+			<PlatformCard
+				icon={<FaGitlab />}
+				title="Gitlab"
+				isDisabled={false}
+				description="Lorem ipsum dolor sit amet."
+				isSelected={false}
+			/>
+			<PlatformCard
+				icon={<FaGitlab />}
+				title="Gitlab"
+				isDisabled={false}
+				description="Lorem ipsum dolor sit amet."
+				isSelected={false}
+			/>
+		</div>
+	);
 }
 
 export default SuportedPlatforms;
